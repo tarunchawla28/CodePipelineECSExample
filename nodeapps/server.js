02f962097e514775bb0595f75daacab770e44805
Tarun Chawla <37270001+tarunchawla28@users.noreply.github.com>
2019-03-06 01:20:25 +0530

    Update server.js

--- nodeapps/server.js
+++ nodeapps/server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     logger.debug({ req: req.originalUrl, method: req.method }, 'Received a Request');
     logger.info({ req: req.originalUrl, method: req.method, resStatus: res.statusCode }, 'Sending Response');
     logger.error('There was no Error');
-    res.send('Welcome to Osmosis 27 Feb 2019!!.. I am Tarun Chawla. Today is the demo day!!!HI Basha\n');
+    res.send('Hi all, I am Tarun Chawla');
 });
 
 app.listen(PORT, HOST);
